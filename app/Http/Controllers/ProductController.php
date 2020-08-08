ac37da14184d8072187b0c08269b0e5700a1f04e
jeovann <jeovann@hotmail.com>
2020-08-07 22:25:26 -0400

    First Commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -30,9 +30,11 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $teste = '<h1>Olá!</h1>';
+        $teste = 123;
+        $teste2 = 123;
+        $teste3 = [];
 
-        return view('admin.pages.products.index', compact('teste'));
+        return view('admin.pages.products.index', compact('teste', 'teste2', 'teste3'));
     }
 
     /**
--- resources/views/Admin/pages/products/index.blade.php
+++ resources/views/Admin/pages/products/index.blade.php
@@ -7,5 +7,53 @@
 @section('content')
     <h1>Exibindo os produtos</h1>
 
-    {{ $teste }}
+    @if ($teste === 123)
+        é igual
+    @elseif($teste == 123)
+        é igual a 123
+    @else
+        é diferente    
+    @endif
+
+    @unless ($teste === '123')
+        Só entra aqui se for falso
+    @else
+        SÓ ENTRA AQUI SE FOR FALSO
+    @endunless
+
+    @isset($teste2)
+        <p>{{$teste2}}</p>
+        
+    @endisset
+
+    @empty($teste3)
+        <p>Vazio...</p>        
+    @endempty
+
+    @auth
+        <P>Autenticado</P>
+    @else
+        <p>Não autenticado</p>
+    @endauth
+
+    @guest
+        <p>Não autenticado</p> 
+    @endguest
+
+    @switch($teste)
+        @case(1)
+            Igual 1
+            @break
+        @case(2)
+            Igual 2
+            @break
+        @case(3)
+            Igual 3
+            @break
+        @case(123)
+            Igual 123
+            @break    
+        @default
+            Default
+    @endswitch
 @endsection
